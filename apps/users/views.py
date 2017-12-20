f377bd72822e3cc249bf51fe9cff6f28c6c9a8c1
Palameng <506357902@qq.com>
2017-12-20 21:15:35 +0800

    2017/12/20

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,17 +2,24 @@
 from django.shortcuts import render
 from django.views.generic import View
 from .models import UserProfile
-from projects.models import Projects
+from projects.models import Projects, ProjectUsers
 # Create your views here.
 
 
 class IndexView(View):
+    # 进入系统后的首页
     def get(self, request):
-        user = UserProfile.objects.get(username='admin')
+        user = UserProfile.objects.get(username=request.user.username)
 
-        sum_of_project = Projects.objects.all().count()
+        if not user:
+            raise Exception
 
-        return render(request, 'index.html', {
+        project_user_list = ProjectUsers.objects.filter(user=user)
+
+        # sum_of_project = Projects.objects.all().count()
+
+        return render(request, 'main_panel.html', {
             "user": user,
-            "sum_of_project": sum_of_project
+            # "sum_of_project": sum_of_project,
+            "project_user_list": project_user_list,
         })
